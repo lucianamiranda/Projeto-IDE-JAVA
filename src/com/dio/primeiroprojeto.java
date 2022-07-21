ee04037fa581ee5a754d38916d9f54edca702566
luciandamiranda <lucianadantasdemiranda@gmail.com>
2022-07-21 19:46:52 -0300

    Implementando classe no projeto

---
+++ src/com/dio/primeiroprojeto.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class primeiroprojeto {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+
+		 System.out.println(gato);
+	}
+
+}
